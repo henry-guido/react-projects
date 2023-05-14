a4a0ca649c8aab203eabbe27d83aa094a5d54a27
Henry Guido <henry_guido@hotmail.com>
2023-05-14 13:32:09 -0600

    fix image

--- src/projects/testimony/components/Testimony.jsx
+++ src/projects/testimony/components/Testimony.jsx
@@ -1,11 +1,11 @@
 import styles from "../style/testimony.module.css";
 
-export function Testimony({person,text,image}) {
+export function Testimony({person,text}) {
 
   return (
     <>
      <div className={styles.testimonyContainer} >
-        <img className={styles.testimonyImage} src={`../images/testimony-${person.image}.png`} alt={`photo from ${person.image}`} />
+        <img className={styles.testimonyImage} src={`./images/testimony-${person.image}.png`} alt={`photo from ${person.image}`} />
         <div className={styles.testimonyContainerText}>
           <p className={styles.testimonyName}>{person.name} en <strong>{person.country}</strong></p>
           <p className={styles.testimonyPosition}>{person.position} en <strong>{person.company}</strong></p>
