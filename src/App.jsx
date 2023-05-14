b6ced4385d0f7d85705c127b011e9cd05cebbf9f
Henry Guido <henry_guido@hotmail.com>
2023-05-14 11:49:15 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,10 @@ import AppCalculator from "./projects/calculator/AppCalculator";
 import AppToDoList from "./projects/todolist/AppToDoList";
 import Home from "./Home";
 import { Routes, Route, Link } from "react-router-dom";
-import LogoHome from "./LogoHome";
-import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <>
-      <header>
-        <Link to="/">
-          <div className={styles.headerContainer}>
-            <div className={styles.logoHome}>
-              <LogoHome />
-            </div>
-            ReactJS Projects
-          </div>
-        </Link>
-      </header>
-      <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/calculator" element={<AppCalculator />} />
@@ -28,7 +15,6 @@ function App() {
           <Route path="/testimony" element={<AppTestimony />} />
           <Route path="/todolist" element={<AppToDoList />} />
         </Routes>
-      </main>
     </>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import styles from "./styles/App.module.css";
-
+import logo from "/logo.svg"
 
 const Home = () => {
   return (
     <>
       <main className={styles.mainContainer}>
+      <header>
+        <Link to="/">
+          <div className={styles.headerContainer}>
+            <div className={styles.logoHome}>
+              <img src={logo} alt="" />
+            </div>
+            ReactJS Projects
+          </div>
+        </Link>
+      </header>
         <article>
           <Link to="/calculator">Calculator</Link>
           <img
